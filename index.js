0528f5f7ff8ff4d37a0f180bfc264840d881bd8c
Riverside Rocks <trent@riverside.rocks>
2020-10-03 19:13:32 -0400

    It works!

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const word = require('./word.js');
 
+var chunk = '["rearouse","jerkily","transductions","phrasing","liftgate","rumbustiousness","retrospects","nephologies","mueslis","urostyle","elutriation","resects","tetrameter","janty","hardiest","psychometrics","unquestionably","warfare","scolding","menudo","phytohormones","hematologies","staling","patamars","godlily","pamphlet","industrially","door","branded","curates"]'
+
+
+var parse = JSON.parse(chunk);
+
+var count = 0;
+for(var i = 0; i < parse.length; ++i){
+    if(parse[i] == 2)
+        count++;
+}
+
+function getRandomWord() {
+	var random = Math.floor((Math.random() * 30) + 0);
+	return parse[random];
+}
+
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').build();
   try {
     await driver.get('https://www.bing.com/?q=get_ready');     
-
-	// This gives you 100 seconds to sign in so your searches count.
-    await driver.wait(until.titleIs('asdasdas'), 10000);
     var one = 1;
     while(1 == one){
-        var goto = word.getRandomWord();
+        var goto = getRandomWord();
         await driver.get('https://www.bing.com/?q='+goto);
-        await driver.wait(until.titleIs(goto+' - bing'), 3000);
+        await driver.wait(until.titleIs(goto+' - Bing'), 3000);
     }
   } finally {
     await driver.quit();
