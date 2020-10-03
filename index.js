2ab4d48e0852c7cab2108a41510b1da597bfecc9
Riverside Rocks <trent@riverside.rocks>
2020-10-03 18:29:28 -0400

    Test search

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').build();
   try {
-    await driver.get('https://www.bing.com');
-    await driver.findElement(By.id('sb_form'))
-	.sendKeys('webdriver', Key.RETURN);
+    await driver.get('https://www.bing.com/?q=webdrive');
     await driver.wait(until.titleIs('webdriver - Bing'), 10000);
   } finally {
     await driver.quit();
