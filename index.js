7e26729df134055ca5e0d4b085747c1ca43b6ab8
Riverside Rocks <trent@riverside.rocks>
2020-10-03 18:32:59 -0400

    Change back to bing so searches work

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('firefox').build();
   try {
-    await driver.get('https://www.google.com/search?q=webdrive');
-    await driver.wait(until.titleIs('webdriver - Bing'), 10000);
+    await driver.get('https://www.bing.com/?q=Please_sign_in');
+	// This gives you 100 seconds to sign in so your searches count.
+    await driver.wait(until.titleIs('asdasdas'), 100000);
   } finally {
     await driver.quit();
   }
